c71edb2c9fbd9b5e17675bd2aa2cedfc02c72caf
kannchp <s6606021630029@email.kmutnb.ac.th>
2025-02-19 15:19:27 +0700

    doneMongo

--- src/MongoCRUD.js
+++ src/MongoCRUD.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 // Connect to the MongoDB database
-mongoose.connect('mongodb://admin:FNCfxk87428@node71656-nichapa.proen.app.ruk-com.cloud:27017', {
+mongoose.connect('mongodb://admin:LQZkkg73929@node71378-node267tue.proen.app.ruk-com.cloud:11722', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
